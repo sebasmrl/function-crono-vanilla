4b622116e11f39c8fcedbf7dbc195da220098547
Sebastian Morales <davesebastian99@gmail.com>
2022-07-28 08:53:25 -0500

    1er commit Funcion Cronometro en vanilla.js

---
+++ README.md
@@ -0,0 +1,6 @@
+# Funcion Cronometro
+Este repositorio trata de una funcion que cronometro que recibe el tiempo del cronometro,
+el selector del elementoContenedor, el nodo de el reloj, y la callback action en donde
+se configura la tranformacion de el reloj cada segundo hasta que el tiempo sea cero.
+
+La funcion internamente hace un unmount del reloj cuando llega a cero
---
+++ js.js
@@ -0,0 +1,45 @@
+
+const cronometroIni = ( minutos = 1000, contentElement, reloj , accion=({contentElement, reloj, contador})=>{} ) => { 
+    let contador =minutos/1000; 
+    contentElement.appendChild(reloj);
+    const idSetInterval = setInterval(()=>{
+        accion({contentElement, reloj, contador}); //envio obj
+        contador--;
+
+        if(contador <= 0) {
+            clearInterval(idSetInterval);
+            //unmount - desmontar
+            contentElement.removeChild(reloj);
+        }
+    }, 1000); //se activa cada segundo
+}
+
+
+
+
+const elementoContenedor = document.getElementById('relojes')
+
+const createCrono = (time,elementoContenedor, idCronometro)=>{
+    //createCronoeloj
+    const reloj = document.createElement('h1');
+    reloj.textContent = 'Usuario'+idCronometro;
+    
+
+    cronometroIni(time, elementoContenedor, reloj,  ({contador})=>{ //desestructuracion de obj mandado en la callback
+        //defina la tranformacion del reloj
+        reloj.textContent = reloj.textContent +' | '+contador 
+    });
+
+}
+
+createCrono(5000, elementoContenedor, 1);
+createCrono(3000, elementoContenedor, 2);
+createCrono(2000, elementoContenedor, 3);
+createCrono(4000, elementoContenedor, 4);
+
+
+const btnCreateCrono =document.getElementById('crono');
+btnCreateCrono.addEventListener('click', (e)=>{
+createCrono(3000, elementoContenedor, 'FromClick');
+
+})
